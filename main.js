ca32a0a54e1e9f4c0ea8619bf0b6d7db85d17307
kassanj <kassandram022@gmail.com>
2019-08-29 08:56:48 -0700

    style it up!

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ let numTimers;
 const form = document.getElementById('form');
 form.addEventListener('submit', addNewTimer);
 
+
+function getTimeToEvent(reminderDate) {
+  const now = new Date();
+  const diff = reminderDate - now;
+  let seconds = diff / 1000;
+  let minutes = seconds / 60;
+  let hours = minutes / 60;
+  const days = Math.floor(hours / 24);
+  hours = Math.floor(hours - days * 24);
+  minutes = Math.floor(minutes - hours * 60 - days * 1440);
+  seconds = Math.floor(seconds - minutes * 60 - hours * 3600 - days * 86400);
+  return [days, hours, minutes, seconds];
+}
+
+
 function addNewTimer(event) {
   console.log('hello');
 
@@ -12,29 +27,64 @@ function addNewTimer(event) {
 
   event.preventDefault();
 
+  // CREATE UL LIST TAG
+  const unorderList = document.createElement('ul');
+  unorderList.classList.add('collection');
+  unorderList.classList.add('collection');
+
+  // CREATE NEW LIST TAG
+  const listItem = document.createElement('li');
+  listItem.classList.add('collection-item');
+  listItem.classList.add('avatar');
+
+  //   <i class="material-icons circle">av_timer</i>
+  const icon = document.createElement('i');
+  icon.classList.add('material-icons');
+  icon.classList.add('circle');
+  icon.innerHTML = 'av_timer';
+  listItem.appendChild(icon);
+
+  // APPEND TITLE TO THE DOM
   const timerTitle = document.getElementById('title').value;
+  const title = document.createElement('span');
+  title.classList.add('title');
+  title.innerHTML = timerTitle;
+  listItem.appendChild(title);
   document.getElementById('title').value = '';
+
+  // APPEND DESCRIPTION TO THE DOM
   const timerDescription = document.getElementById('description').value;
+  const description = document.createElement('p');
+  description.innerHTML = timerDescription;
+  listItem.appendChild(description);
   document.getElementById('description').value = '';
+
+  // CALCULATE DATETIME FORMAT
   const timerDate = document.getElementById('date').value;
-  document.getElementById('date').value = '';
   const timerTime = document.getElementById('time').value;
+
+  document.getElementById('date').value = '';
   document.getElementById('time').value = '';
 
   const datetime = `${timerDate} ${timerTime}:00`;
-  const timerDatetime = datetime; /// NEWEST FORMAT
+  const timerDatetime = datetime;
 
-  const newTitle = document.createElement('div');
-  newTitle.innerHTML = timerTitle;
-  document.getElementById('reminderList').appendChild(newTitle);
+  // APPEND TARGET DATETIME TO THE DOM
+  const targetDate = document.createElement('p');
+  targetDate.innerHTML = timerDatetime;
+  listItem.appendChild(targetDate);
 
-  const newDescription = document.createElement('div');
-  newDescription.innerHTML = timerDescription;
-  document.getElementById('reminderList').appendChild(newDescription);
+  // APPEND COUNTDOWN TAG TO THE DOM
+  const countdown = document.createElement('div');
+  countdown.classList.add('secondary-content');
+  countdown.id = 'reminder_1'; // MAKE DYNAMIC
+  countdown.innerHTML = '00:00:00:0'; // LEAVE EMPTY? OR CALL FUNCTION?
+  listItem.appendChild(countdown);
 
-  const newDatetime = document.createElement('div');
-  newDatetime.innerHTML = timerDatetime;
-  document.getElementById('reminderList').appendChild(newDatetime);
+  // APPEND NEW LIST ITEM TO THE UL
+  unorderList.appendChild(listItem)
+  document.getElementById('reminderList').appendChild(unorderList);
+
 
   chrome.storage.sync.set({ [`reminder${numTimers}`]: { title: timerTitle, description: timerDescription, datetime: timerDatetime } }, (reminder) => {
     console.log(`New reminder: ${timerTitle} ${timerDescription} ${timerDatetime}`);
@@ -52,17 +102,49 @@ document.addEventListener('DOMContentLoaded', () => {
       if (keys[i] === 'numTimers') numTimers = store[keys[i]];
       else {
 
-        const title = document.createElement('div');
+        // CREATE UL LIST TAG
+        const unorderList = document.createElement('ul');
+        unorderList.classList.add('collection');
+        unorderList.classList.add('collection');
+
+        // CREATE NEW LIST TAG
+        const listItem = document.createElement('li');
+        listItem.classList.add('collection-item');
+        listItem.classList.add('avatar');
+
+        //   <i class="material-icons circle">av_timer</i>
+        const icon = document.createElement('i');
+        icon.classList.add('material-icons');
+        icon.classList.add('circle');
+        icon.innerHTML = 'av_timer';
+        listItem.appendChild(icon);
+
+        // APPEND TITLE TO THE DOM
+        const title = document.createElement('span');
+        title.classList.add('title');
         title.innerHTML = store[keys[i]].title;
-        document.getElementById('reminderList').appendChild(title);
+        listItem.appendChild(title);
 
-        const description = document.createElement('div');
+        // APPEND DESCRIPTION TO THE DOM
+        const description = document.createElement('p');
         description.innerHTML = store[keys[i]].description;
-        document.getElementById('reminderList').appendChild(description);
+        listItem.appendChild(description);
 
-        const datetime = document.createElement('div');
+        // APPEND TARGET DATETIME TO THE DOM
+        const datetime = document.createElement('p');
         datetime.innerHTML = store[keys[i]].datetime;
-        document.getElementById('reminderList').appendChild(datetime);
+        listItem.appendChild(datetime);
+
+        // APPEND COUNTDOWN TO THE DOM
+        const countdown = document.createElement('div');
+        countdown.classList.add('secondary-content');
+        countdown.id = 'reminder_1'; // MAKE DYNAMIC
+        countdown.innerHTML = '00:00:00:0'; // LEAVE EMPTY? OR CALL FUNCTION?
+        listItem.appendChild(countdown);
+
+        unorderList.appendChild(listItem)
+        document.getElementById('reminderList').appendChild(unorderList);
+        
       }
     }
     if (numTimers === undefined) numTimers = 0;
@@ -87,4 +169,19 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   var elems = document.querySelectorAll('.timepicker');
   var instances = M.Timepicker.init(elems, options);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  setInterval(() => {
+    const allCountdowns = document.getElementsByClassName('countdown');
+    for (const downer of allCountdowns) {
+      /* const newCountdown = document.createElement('div');
+      newCountdown.innerHTML = JSON.stringify(getTimeToEvent(new Date(downer.innerHTML)));
+      document.getElementById('reminderList').appendChild(newCountdown);
+      if (newCountdown.innerHTML === '[0,0,0,0]') alert('FUCK YEA');
+      //console.log(newCountdown.innerHTML); */
+    }
+  }, 1000);
 });
